1a4885d1217cc8cbd6594b279d143c7440916652
Sam <sjhewitt123@hotmail.com>
2019-10-10 16:45:12 +1300

    motorcycle of the week json and javascript files added

---
+++ assets/js/featured.js
@@ -0,0 +1,29 @@
+const items = document.getElementById('items');
+const list = document.getElementById('item-list');
+
+const xhr = new XMLHttpRequest();
+
+
+    xhr.open('GET', 'featured.json', true);
+    xhr.onload = () => {
+        if (xhr.status === 200){
+            const per = JSON.parse(xhr.responseText);
+            //console.log(per);
+
+            per.forEach(p => {
+                 let list = document.getElementById('item-list');
+
+            list.innerHTML = list.innerHTML + `<br>${p.id}<br><img src="${p.img}" height="300" width="420"></img><br>${p.bikeName}<br> ${p.bikeType}<br> ${p.bikeSummary} </br>`;
+            })
+        }
+            else if (xhr.status === 404){
+            console.log("File Not Found");
+        }
+    }
+
+    xhr.onerror = () =>{
+        console.log(`Current Load State ${xhr.readyState}`);
+    };
+
+    xhr.send();
+
