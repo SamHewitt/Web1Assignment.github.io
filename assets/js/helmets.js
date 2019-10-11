3802029d7af2b341565eb0032e55e439ad0d4404
Sam <sjhewitt123@hotmail.com>
2019-10-12 09:21:34 +1300

    added review functionality to motorcycles,pants and helmets pages

--- assets/js/helmets.js
+++ assets/js/helmets.js
@@ -3,53 +3,56 @@ const list = document.getElementById('item-list');
 
 const xhr = new XMLHttpRequest();
 
-
+    // displays json file to html page
     xhr.open('GET', 'helmets.json', true);
     xhr.onload = () => {
         if (xhr.status === 200){
             const per = JSON.parse(xhr.responseText);
             //console.log(per);
-
             per.forEach(p => {
                  let list = document.getElementById('item-list');
 
-            list.innerHTML = list.innerHTML + `<br>${p.id}<br><img src="${p.img}" height="200" width="200"></img> <br>${p.helmetName}<br> ${p.helmetType}<br> ${p.helmetSummary} </br>`;
+            list.innerHTML = list.innerHTML + `<br>${p.id}<br><img src="${p.img}" height="200" width = "200"></img> <br>${p.helmetName}<br> ${p.helmetType}<br> ${p.helmetSummary} </br>`;
             })
         }
             else if (xhr.status === 404){
             console.log("File Not Found");
         }
     }
-
     xhr.onerror = () =>{
         console.log(`Current Load State ${xhr.readyState}`);
     };
 
     xhr.send();
-  // php to page
-  const sendPhp = e => {
-    e.preventDefault();
-  
-    const Name = document.getElementById("Name").value;
-    const Review = document.getElementById("Review").value;
-    const params = `Name=${Name}&Review=${Review}`;
-    xhr.open("POST", "http://kate.ict.op.ac.nz/~orrgl1/server.php", true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        console.log(xhr.responseText);
-        let text = document.getElementById("ajax-php-text");
-        text.innerHTML = `<p>${xhr.responseText}</p>`;
-        text.style.color = "red"
-        text.style.fontSize = "25px"
-        text.style.border = "thick solid #fff"
-        text.style.background = "#455"
-      } else if (xhr.status === 500) {
-        console.log("Internal Server Error.");
-      }
-    };
-    xhr.send(params);
-  };
 
-document.getElementById("f1").addEventListener("submit", sendPhp);
-document.getElementById("ajax-php-button").addEventListener("click", sendPhp);
+    // display comment to html page
+
+    const showComment = () => {
+  
+      const nameVal = document.getElementById('name-input').value
+      const itemVal = document.getElementById('item-input').value
+      const commentVal = document.getElementById('comment-input').value
+      const datetime = document.lastModified
+      const form = document.getElementById('f1')
+      if (nameVal.length <= 0){
+        alert("please enter a name")
+      } 
+      if (itemVal.length <= 0){
+        alert("please enter an Item")
+      }   
+      if (commentVal.length <= 0){
+        alert("please enter a comment")
+      }
+      if (((nameVal.length >= 1)&&(itemVal.length >= 1) && (commentVal.length >= 1)))
+      {
+      form.innerHTML =
+        `<div id = "p2">  
+        <p>Name:${nameVal}</p><br>
+        <p>Item:${itemVal}</p><br>
+        <p>Review:${commentVal}</p><br>
+        <p>Date:${datetime}</p><br>
+        </div>` + form.innerHTML;
+      }
+
+     }
+     document.getElementById('comment-data').addEventListener('click', showComment)
