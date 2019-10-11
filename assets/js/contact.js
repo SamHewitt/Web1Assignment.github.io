41c9c476b964f2ffe146228988aaaaa2a7f14b09
Sam <sjhewitt123@hotmail.com>
2019-10-12 10:32:42 +1300

    prototype star functionality

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -19,8 +19,9 @@ const showComment = () => {
     }
     if (((nameVal.length >= 1) && (emailVal.length >= 1) && (phoneVal.length >= 1) && (commentVal.length >= 1)))
     {
-  
-      alert("Thankyou for your message, " + nameVal + "!")
+     
+      alert("Thankyou for your message, " + nameVal + "!") 
+
     }
 }
 document.getElementById('comment-data').addEventListener('click', showComment)
