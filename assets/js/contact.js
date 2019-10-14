d9257da12bf0ea6a544143748bbe28466b7aa7e5
Sam <sjhewitt123@hotmail.com>
2019-10-15 11:11:33 +1300

    page tidy ups using prettier extension

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,30 +1,16 @@
-
 const showComment = () => {
-    const nameVal = document.getElementById('contact-input').value
-    const emailVal = document.getElementById('email-input').value
-    const phoneVal = document.getElementById('phone-input').value
-    const commentVal = document.getElementById('message-input').value
-    // if (nameVal.length <= 0){
-    //   alert("please enter a Name")
-    // } 
-    // if (emailVal.length <= 0){
-    //   alert("please enter an email address")
-    // }   
-    // if (phoneVal.length <= 0){
-    //     alert("please enter your phone number")
-    //   } 
-    // if (commentVal.length <= 0){
-    //   alert("please enter a comment")
-    // }
-    if (((nameVal.length >= 1) && (emailVal.length >= 1) && (phoneVal.length >= 1) && (commentVal.length >= 1)))
-    {
-     
-      alert("Thankyou for your message, " + nameVal + "!") 
+  const nameVal = document.getElementById("contact-input").value;
+  const emailVal = document.getElementById("email-input").value;
+  const phoneVal = document.getElementById("phone-input").value;
+  const commentVal = document.getElementById("message-input").value;
 
-    }
-}
-document.getElementById('comment-data').addEventListener('click', showComment)
-
-
-
-
+  if (
+    nameVal.length >= 1 &&
+    emailVal.length >= 1 &&
+    phoneVal.length >= 1 &&
+    commentVal.length >= 1
+  ) {
+    alert("Thankyou for your message, " + nameVal + "!");
+  }
+};
+document.getElementById("comment-data").addEventListener("click", showComment);
--- assets/js/pants.js
+++ assets/js/pants.js
@@ -1,87 +1,85 @@
-const items = document.getElementById('items');
-const list = document.getElementById('item-list');
+const items = document.getElementById("items");
+const list = document.getElementById("item-list");
 
 const xhr = new XMLHttpRequest();
 
-    // displays json file to html page
-    xhr.open('GET', 'pants.json', true);
-    xhr.onload = () => {
-        if (xhr.status === 200){
-            const per = JSON.parse(xhr.responseText);
-            //console.log(per);
-            per.forEach(p => {
-                 let list = document.getElementById('item-list');
+// displays json file to html page
+xhr.open("GET", "pants.json", true);
+xhr.onload = () => {
+  if (xhr.status === 200) {
+    const per = JSON.parse(xhr.responseText);
+    //console.log(per);
+    per.forEach(p => {
+      let list = document.getElementById("item-list");
 
-            list.innerHTML = list.innerHTML + `<div class="item-listing"><br>${p.id}<br><img src="${p.img}" height="200" width = "200"></img> <br>${p.pantsName}<br> ${p.pantsType}<br> ${p.pantsSummary}</div></br>`;
-            })
-        }
-            else if (xhr.status === 404){
-            console.log("File Not Found");
-        }
-    }
-    xhr.onerror = () =>{
-        console.log(`Current Load State ${xhr.readyState}`);
-    };
+      list.innerHTML =
+        list.innerHTML +
+        `<div class="item-listing"><br>${p.id}<br><img src="${p.img}" height="200" width = "200"></img> <br>${p.pantsName}<br> ${p.pantsType}<br> ${p.pantsSummary}</div></br>`;
+    });
+  } else if (xhr.status === 404) {
+    console.log("File Not Found");
+  }
+};
+xhr.onerror = () => {
+  console.log(`Current Load State ${xhr.readyState}`);
+};
 
-    xhr.send();
+xhr.send();
 
-    // display comment to html page
+// display comment to html page
 
-    const showComment = () => {
-  
-      const nameVal = document.getElementById('name-input').value
-      const itemVal = document.getElementById('item-input').value
-      const commentVal = document.getElementById('comment-input').value
-      const datetime = new Date();
-      const form = document.getElementById('f1')
-      if (nameVal.length <= 0){
-        alert("please enter a name")
-      } 
-      if (itemVal.length <= 0){
-        alert("please enter an Item")
-      }   
-      if (commentVal.length <= 0){
-        alert("please enter a comment")
-      }
-      if (((nameVal.length >= 1)&&(itemVal.length >= 1) && (commentVal.length >= 1)))
-      {
-      form.innerHTML =
-        `<div id = "p2">  
+const showComment = () => {
+  const nameVal = document.getElementById("name-input").value;
+  const itemVal = document.getElementById("item-input").value;
+  const commentVal = document.getElementById("comment-input").value;
+  const datetime = new Date();
+  const form = document.getElementById("f1");
+  if (nameVal.length <= 0) {
+    alert("please enter a name");
+  }
+  if (itemVal.length <= 0) {
+    alert("please enter an Item");
+  }
+  if (commentVal.length <= 0) {
+    alert("please enter a comment");
+  }
+  if (nameVal.length >= 1 && itemVal.length >= 1 && commentVal.length >= 1) {
+    form.innerHTML =
+      `<div id = "p2">  
         <p>Name:${nameVal}</p><br>
         <p>Item:${itemVal}</p><br>
         <p>Review:${commentVal}</p><br>
         <p>Date:${datetime}</p><br>
         </div>` + form.innerHTML;
-      }
+  }
+};
+document.getElementById("comment-data").addEventListener("click", showComment);
 
-     }
-     document.getElementById('comment-data').addEventListener('click', showComment)
-
-     // Star Rating 
+// Star Rating
 // Initial Ratings
 const ratings = {
   rhino: 0,
   short: 0,
   astars: 0,
   fly: 0,
- astarsM: 0
-}
+  astarsM: 0
+};
 
 // Total Stars
 const starsTotal = 5;
 
 // Run getRatings when DOM loads
-document.addEventListener('DOMContentLoaded', getRatings);
+document.addEventListener("DOMContentLoaded", getRatings);
 
 // Form Elements
-const productSelect = document.getElementById('product-select');
-const ratingControl = document.getElementById('rating-control');
+const productSelect = document.getElementById("product-select");
+const ratingControl = document.getElementById("rating-control");
 
 // Init product
 let product;
 
 // Product select change
-productSelect.addEventListener('change', (e) => {
+productSelect.addEventListener("change", e => {
   product = e.target.value;
   // Enable rating control
   ratingControl.disabled = false;
@@ -89,12 +87,12 @@ productSelect.addEventListener('change', (e) => {
 });
 
 // Rating control change
-ratingControl.addEventListener('blur', (e) => {
+ratingControl.addEventListener("blur", e => {
   const rating = e.target.value;
 
   // Make sure 5 or under
   if (rating > 5) {
-    alert('Please rate 1 - 5');
+    alert("Please rate 1 - 5");
     return;
   }
 
@@ -114,9 +112,12 @@ function getRatings() {
     const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
 
     // Set width of stars-inner to percentage
-    document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+    document.querySelector(
+      `.${rating} .stars-inner`
+    ).style.width = starPercentageRounded;
 
     // Add number rating
-    document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
+    document.querySelector(`.${rating} .number-rating`).innerHTML =
+      ratings[rating];
   }
 }
